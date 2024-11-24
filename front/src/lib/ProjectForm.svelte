6ba0ae82e067f862d918f3c8c4cf9e0309f3dfcb
Steffew <xgenyofficial@gmail.com>
2024-11-24 20:47:26 +0100

    Make scrollbar appear when content is too big

--- front/src/lib/ProjectForm.svelte
+++ front/src/lib/ProjectForm.svelte
@@ -31,7 +31,7 @@
       <Tinymce bind:value={content} class="w-full" />
     </div>
     <div class="flex justify-end mt-8">
-      <button type="submit" class="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition">Save Project</button>
+      <button type="submit" class="bg-blue-500 text-white px-6 py-3 rounded hover:bg-blue-600 transition">Save Project</button>
     </div>
   </form>
 </div>
--- front/src/routes/admin/new/+page.svelte
+++ front/src/routes/admin/new/+page.svelte
@@ -2,8 +2,8 @@
     import ProjectForm from '$lib/ProjectForm.svelte';
   </script>
   
-  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
-    <div class="w-full md:w-5/6 max-w-5xl">
+  <div class="fixed inset-0 flex items-start justify-center bg-black bg-opacity-50 overflow-y-auto pt-3 pb-4">
+    <div class="w-full md:w-5/6 max-w-5xl mt-3">
       <ProjectForm />
     </div>
   </div>
