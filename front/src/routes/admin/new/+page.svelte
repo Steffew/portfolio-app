6af91fc0b13f0a45e5da1e06474415b506db4ef8
Steffew <xgenyofficial@gmail.com>
2024-11-24 20:21:15 +0100

    Add ProjectForm component

---
+++ front/src/routes/admin/new/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import ProjectForm from '$lib/ProjectForm.svelte';
+  </script>
+  
+  <div class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-30">
+    <div class="w-full md:w-5/6 max-w-5xl">
+      <ProjectForm />
+    </div>
+  </div>
+  
+  <style>
+  </style>
+  
