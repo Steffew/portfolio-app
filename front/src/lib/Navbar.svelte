0fe29a8d9ac603e5db2f185bb49743b944e494ec
Steffew <xgenyofficial@gmail.com>
2024-11-24 17:41:45 +0100

    Create simple navbar

---
+++ front/src/lib/Navbar.svelte
@@ -0,0 +1,29 @@
+<nav class="flex items-center justify-between bg-white bg-opacity-5 p-3">
+	<div class="text-xl lg:ms-20">
+		<p>Stw</p>
+	</div>
+
+	<ul class="flex w-full items-center justify-center space-x-4 md:w-auto lg:me-20 lg:justify-end">
+		<li>
+			<a
+				href="/"
+				class="rounded border border-white border-opacity-25 bg-transparent px-3 py-2 hover:border-opacity-50"
+				>Home</a
+			>
+		</li>
+		<li>
+			<a
+				href="/portfolio"
+				class="rounded border border-white border-opacity-25 bg-transparent px-3 py-2 hover:border-opacity-50"
+				>Portfolio</a
+			>
+		</li>
+		<li>
+			<a
+				href="/about"
+				class="rounded border border-white border-opacity-25 bg-transparent px-3 py-2 hover:border-opacity-50"
+				>About</a
+			>
+		</li>
+	</ul>
+</nav>
