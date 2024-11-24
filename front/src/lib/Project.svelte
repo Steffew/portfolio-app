3fc39e61aea5476d886ee6c30673ffaec532ead1
Steffew <xgenyofficial@gmail.com>
2024-11-24 19:38:28 +0100

    Improve gradient range

--- front/src/lib/Project.svelte
+++ front/src/lib/Project.svelte
@@ -12,7 +12,7 @@
     class="relative bg-black bg-opacity-50 border border-white border-opacity-20 rounded-lg shadow-md overflow-hidden transition-transform transform hover:shadow-lg w-auto m-1 group"
   >
     <img src={imageUrl} alt={title} class="w-full h-64 object-cover transition-transform transform group-hover:scale-110 duration-500 ease-out" />
-    <div class="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent h-2/4 flex flex-col justify-end">
+    <div class="absolute bottom-0 left-0 right-0 p-4 bg-gradient-to-t from-black to-transparent h-2/3 flex flex-col justify-end">
       <h2 class="text-xl font-bold text-white">{title}</h2>
       <p class="text-sm text-gray-300">{description}</p>
     </div>
