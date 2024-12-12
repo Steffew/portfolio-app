9779531f1cd3fa17fef0a7b2dccb411f4918149c
Steffew <xgenyofficial@gmail.com>
2024-12-12 11:20:00 +0100

    Update connection string

--- back/database/db.js
+++ back/database/db.js
@@ -4,13 +4,13 @@ const connectDB = async () => {
   console.log("Attempting to connect to MongoDB...");
   try {
     await mongoose.connect(
-      "mongodb://admin:adminpassword@localhost:27017/projects"
+      "mongodb://admin:adminpassword@localhost:27017/portfolio?authSource=admin"
     );
     console.log("MongoDB connected successfully!");
   } catch (error) {
     if (error.message.includes("Authentication failed")) {
       console.error(
-        "Authentication failed: Check if the 'projects' database exists and the credentials are correct."
+        "Authentication failed: Check if the 'portfolio' database exists and the credentials are correct."
       );
     } else {
       console.error("Error connecting to MongoDB:", error.message);
