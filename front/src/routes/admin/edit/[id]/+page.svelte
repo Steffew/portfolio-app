0948b0a32ffa3c642e1bc952d116188ac7db9281
Steffew <xgenyofficial@gmail.com>
2024-12-15 05:56:15 +0100

    Update structure of edit page

---
+++ front/src/routes/admin/edit/[id]/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import ProjectForm from '$lib/ProjectForm.svelte';
+  import { onMount } from 'svelte';
+  import { page } from '$app/stores';
+  import { get } from 'svelte/store';
+
+  let title = '';
+  let description = '';
+  let thumbnailUrl = '';
+  let slug = '';
+  let content = '';
+  let errorMessage: string | null = null;
+
+  onMount(async () => {
+    const id = get(page).params?.id;
+    if (!id) {
+      errorMessage = 'Invalid project ID.';
+      return;
+    }
+    try {
+      const response = await fetch(`http://localhost:5000/api/projects/${id}`);
+      if (!response.ok) throw new Error('Failed to fetch project data');
+      const project = await response.json();
+
+      title = project.title;
+      description = project.description;
+      thumbnailUrl = project.image_url;
+      slug = project.slug;
+      content = project.content;
+    } catch (error) {
+      errorMessage = 'Unable to load project data.';
+    }
+  });
+</script>
+
+{#if errorMessage}
+  <p class="text-red-500 text-center">{errorMessage}</p>
+{:else}
+  <ProjectForm
+    {title}
+    {description}
+    {thumbnailUrl}
+    {slug}
+    {content}
+  />
+{/if}
