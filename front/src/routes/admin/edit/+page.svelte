46865f6c413514cad94c6f10fc2a223d0aad2de3
Steffew <xgenyofficial@gmail.com>
2024-12-15 06:14:46 +0100

    Add placeholder index page for /edit

---
+++ front/src/routes/admin/edit/+page.svelte
@@ -0,0 +1,4 @@
+<div class="w-full h-full p-8 rounded-lg">
+    <h1 class="font-bold text-white mb-6 text-center">No project to edit! Please enter the ID of the project you would like to edit.</h1>
+
+  </div>
