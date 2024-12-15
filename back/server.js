ab63864bf5f66d0bae18cd8a1e78cd81b0b2fb2e
Steffew <xgenyofficial@gmail.com>
2024-12-15 02:20:36 +0100

    Import and enable CORS

--- back/server.js
+++ back/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors"); // Import CORS middleware
 const connectDB = require("./database/db"); // Database connection file
 const projectRoutes = require("./routes/projectRoutes"); // Routes for "projects"
 
 const app = express();
 
 // Middleware
+app.use(cors()); // Enable CORS for cross-origin requests
 app.use(express.json()); // Parse JSON request bodies
 
 // Connect to the database
