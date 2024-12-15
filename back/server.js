65fa1d9e5850c17c2570d8c35c78332e3c9056a3
Steffew <xgenyofficial@gmail.com>
2024-12-15 18:07:27 +0100

    Remove comments

--- back/server.js
+++ back/server.js
@@ -1,21 +1,19 @@
 const express = require("express");
-const cors = require("cors"); // Import CORS middleware
-const connectDB = require("./database/db"); // Database connection file
-const projectRoutes = require("./routes/projectRoutes"); // Routes for "projects"
+const cors = require("cors");
+const connectDB = require("./database/db");
+const projectRoutes = require("./routes/projectRoutes");
 
 const app = express();
 
 // Middleware
-app.use(cors()); // Enable CORS for cross-origin requests
-app.use(express.json()); // Parse JSON request bodies
+app.use(cors());
+app.use(express.json());
 
-// Connect to the database
 connectDB();
 
 // Routes
 app.use("/api/projects", projectRoutes); // Prefix all project routes with /api/projects
 
-// Start the server
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
